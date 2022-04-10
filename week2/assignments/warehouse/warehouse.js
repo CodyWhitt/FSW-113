6dc8e7699c89709b3b2742f75fe0bded54e591b5
CodyWhitt <95008570+CodyWhitt@users.noreply.github.com>
2022-04-10 16:41:34 -0500

    Resubmission Needs help and work

--- week2/assignments/warehouse/warehouse.js
+++ week2/assignments/warehouse/warehouse.js
@@ -21,67 +21,74 @@ const parts = [
     { partNbr: 'WEYPU3Z', partDescr: 'Mumpenflipper', aisle: 'E3', qty: 1},
 ]
 
-// list of each part number and qty for check-off in the "detailsList" element
+// list of each part number and qty for check-off in the "detailsList" element (DONE_)
 
 // if parts requiring special handling exist (in aisle B3), list of items needing 
-// special packaging in the "specialPackaging" element, else remove element
+// special packaging in the "specialPackaging" element, else remove element (DONEish_)
 
 // if hazardous parts exist (in aisle J4), let employee know in the "hazardousMaterials"
-// element and remind them to get gloves, else remove element
+// element and remind them to get gloves, else remove element (DONE_)
 
 // if all items in the order are small parts (aisle H1), then let employee know that they should take 
-// a basket and go dirctly to aisle H1
+// a basket and go dirctly to aisle H1 (DONE_)
 
 // if there are large items (anthing in aisles S, T, or U), then let the employee know in the "forkiftNeeded"
-// element that they will need to reserve a forklift, else remove the element
+// element that they will need to reserve a forklift, else remove the element (DONEish_)
 
-// sum up the total number of parts and append that number to the text already in "totalItems" element
+// sum up the total number of parts and append that number to the text already in "totalItems" element (DONE_)
 
-detailsList = document.getElementById('detailsList')
-specialPackaging = document.getElementById('specialPackaging')
-hazardousMaterials = document.getElementById('hazardousMaterials')
-smallItemsOnly = document.getElementById('smallItemsOnly')
-forkliftNeeded = document.getElementById('forkiftNeeded')
-totalItems = document.getElementById('totalItems')
+let detailsList = '<ol>'
+parts.forEach(function(item){
+    detailsList += `<li><input type= checkbox></input>${item.qty} (${item.partNbr}) - ${item.partDescr}</li>`
+})
+detailsList += '</ol>'
+document.querySelector('#detailsList').innerHTML = detailsList
 
-for (i = 0; i < parts.length; i++) {
-    part = parts[i].partNbr + " |----| " + parts[i].qty
-    toString(part)
-    item = document.createElement('div')
-    item.innerHTML = part
-    detailsList.appendChild(item)
-    checkBox = document.createElement('input')
-    checkBox.setAttribute('type', 'checkbox')
-    item.appendChild(checkBox)
-    if (parts[i].aisle === 'B3'){
-        part = parts[i].partDescr
-        toString(part)
-        item = document.createElement('div')
-        item.innerHTML = part
-        specialPackaging.appendChild(item)
-    }
-    if (parts[i].aisle === 'J4'){
-        part = parts[i].partDescr
-        toString(part)
-        item = document.createElement('div')
-        item.innerHTML = part
-        hazardousMaterials.appendChild(item)
-    }
-    if (parts[i].aisle === 'H1'){
-        part = parts[i].partDescr
-        toString(part)
-        item = document.createElement('div')
-        item.innerHTML = part
-        smallItemsOnly.appendChild(item)
-    }
-    if ((parts[i].aisle === 'S')||(parts[i].aisle === 'T')||(parts[i].aisle === 'U')){
-        part = parts[i].partDescr
-        toString(part)
-        item = document.createElement('div')
-        item.innerHTML = part
-        forkliftNeeded.appendChild(item)
-    }
+const arySpecial = parts.filter(function(parts) {
+    return parts.aisle ==='B3'
+})
+let specialPackaging = '<ol>'
+arySpecial.filter(function(item){
+    specialPackaging += `<li>Item: ${item.partNbr} / Qty:${item.qty}</li>`
+})
+specialPackaging += '</ol>'
+document.querySelector('#specialPackaging').innerHTML = `Special Packaging required` + specialPackaging //issue here doesnt perfectly match text
+//issue here cannot figure out the else portion for .remove() when filter returns nothing. aside from another if statement.
+
+const hazardousMaterials = parts.some(function(item) {
+    return item.aisle ==='J4'
+})
+if (hazardousMaterials) {
+    document.querySelector('#hazardousMaterials').innerText = 'Hazardous Parts Included\n\nGet Gloves'
+} else { 
+    document.querySelector('#hazardousMaterials').remove()
+}
+
+const smallItemsOnly = parts.every(function(item) {
+    return item.aisle ==='H1'
+})
+if (smallItemsOnly) {
+    document.querySelector('#smallItemsOnly').innerText = 'Take a basket and go directly to aisle H1'
+} else {
+    document.querySelector('#smallItemsOnly').remove()
+}
+
+const forkiftNeeded = parts.some(function(item) {
+    return item.aisle === ('S' || 'T' || 'U') //issues here doesnt pick up aisle T || U
+})
+if (forkiftNeeded) {
+    document.querySelector('#forkiftNeeded').innerText = 'Forklift Needed'
+} else { 
+    document.querySelector('#forkiftNeeded').remove()
 }
 
 result = parts.reduce((accumulator, current) => accumulator += current.qty, 0);
 totalItems.insertAdjacentHTML('beforeend', ": " + result);
+
+// forEach() x1
+// filter() ?
+// map() not sure where to use
+// find() not sure where to use
+// some() x2
+// every() x1
+// reduce() x1
