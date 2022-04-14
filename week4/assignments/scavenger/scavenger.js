b2f76927ea180cbdeac6901272c3cf2f6f155c07
CodyWhitt <95008570+CodyWhitt@users.noreply.github.com>
2022-04-13 22:20:42 -0500

    Scavenger Submission

--- week4/assignments/scavenger/scavenger.js
+++ week4/assignments/scavenger/scavenger.js
@@ -1,10 +1,23 @@
-// Create an event listenter for the 'submit' button that calls the combineLists() function
+// Create an event listenter for the 'submit' button that calls the combineLists() function (DONE_)
 
 // Modify the combineLists() function to create arrays from each scavenger hunt element in the scavenger.html document. 
 // Then use the spread operator to combine these arrays into a single array. 
 // Sort that array and display it in the 'AllItems' element on the scavenger.html document.
 
+subButton = document.getElementById('submit')
+subButton.addEventListener('click',combineLists)
+
+
+
 function combineLists() {
+    scavengerAry0 = [document.getElementById('scavenger-0').innerHTML]
+    scavengerAry1 = [document.getElementById('scavenger-1').innerHTML]
+    scavengerAry2 = [document.getElementById('scavenger-2').innerHTML]
+    scavengerAry3 = [document.getElementById('scavenger-3').innerHTML]
+    combinedList = [...scavengerAry0, ...scavengerAry1, ...scavengerAry2, ...scavengerAry3]
+    combinedList.sort()
+    output = document.getElementById('AllItems')
+    output.innerHTML = combinedList
 }
 
 // The following code is a bonus addition that facilitates data entry on the scavenger.html document.
