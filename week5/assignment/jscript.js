5b1301c6467fd33c72d77448af9a12f655a90fb0
CodyWhitt <95008570+CodyWhitt@users.noreply.github.com>
2022-04-27 19:43:44 -0500

    W5A1 Submission

--- week5/assignment/jscript.js
+++ week5/assignment/jscript.js
@@ -1,14 +1,55 @@
 // Declare any necessary variables.
+//  inputs and cert outputs
+const studentName = document.getElementById('studentName')
+const className = document.getElementById('className')
+const certStudentName = document.getElementById('certStudentName')
+const certClassName = document.getElementById('certClassName')
+const certGrade = document.getElementById('certGrade')
+let certStudent;
 
-// Add am evemt listener that responds to the click of the "print" button by calling a function to instantiate
+
+// Add an event listener that responds to the click of the "print" button by calling a function to instantiate
 //  a new student object, and another function to print the certificate.
+document.getElementById('print').addEventListener('click', function () {
+    newStu()
+    certFill()
+})
 
 // Add an event listener that responds to the click of the reset button by resetting all the values
 // both in the form and in the certificate.
+document.getElementById('reset').addEventListener('click', function clearFields(){
+    studentName.value = ''
+    className.value = ''
+    document.getElementById('studentScores').value = ''
+    document.getElementById('possibleScores').value = ''
+    certStudentName.innerHTML = '&nbsp;'
+    certClassName.innerHTML = '&nbsp;'
+    certGrade.innerHTML = '&nbsp;'
+})
 
 // Create a function that instantiates a new student object with the input from the HTML form.
+function newStu(){
+    certStudent = new student (
+        studentName.value, className.value, stringToAry(studentScores.value), stringToAry(possibleScores.value)
+    )
+}
+
+
 
 // Create a function that fills in the student's name, class name, and calculated grade on the certificate .
+function certFill(){
+    certStudentName.innerHTML = certStudent.getStName()
+    certClassName.innerHTML = certStudent.getClName()
+    certGrade.innerHTML = certStudent.letterCalc()
+}
+
 
 // Create a function that converts the contents of a comma-separated text string to a numeric array.
 // You can use this function when instantiating the arrays in the student object.
+function stringToAry(string){
+    ary = string.split(',')
+    ary = ary.map(function (x) {
+        return parseInt(x)
+    })
+    return ary
+}
--- week5/assignment/student.js
+++ week5/assignment/student.js
@@ -2,4 +2,45 @@
 // The class should also contain the following methods:
 // - a method that adds up all the student's scores
 // - a method that adds up all the possible scores
-// - a method that calculates the student's letter grade (divide the student's score by the possible scores and use the resulting decimal to determine grade)
+// - a method that calculates the student's letter grade (divide the student's score by the possible scores and 
+//   use the resulting decimal to determine grade)
+
+class student {
+    constructor(studentName, className, studentScores, possibleScores) {
+    this.studentName = studentName
+    this.className = className
+    this.studentScores = studentScores
+    this.possibleScores = possibleScores
+    }
+
+    getStName() {
+        return this.studentName
+    }
+
+    getClName(){
+        return this.className
+    }
+
+    stuScoreSum() {
+        let result = this.studentScores.reduce((accumulator, current) => accumulator + current, 0);
+        return result
+    } 
+
+    possScoreSum() {
+        let result = this.possibleScores.reduce((accumulator, current) => accumulator + current, 0);
+        return result
+    }
+
+    letterCalc(result) {
+        result = this.stuScoreSum() / this.possScoreSum()
+        if (result >= .9){
+            return 'A'
+        } else if (result >= .8){
+            return 'B'
+        } else if (result >= .7){
+            return 'C'
+        } else {
+            return 'F'
+        }
+    }
+}
